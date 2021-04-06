c2278b4457049be46873c3283f2e5fc678032bb6
102451 <102451@fhwn.ac.at>
2021-04-06 14:39:25 +0200

    Update main to execute the tests

--- Caskell/Main.hs
+++ Caskell/Main.hs
@@ -1,31 +1,5 @@
-import Options.Applicative
-import Data.Semigroup ((<>))
 
-import Caskell.Bytes
 import Caskell.Tests
-import Caskell.Hash
-import Caskell.CoreHash
-import Caskell.Compile
-
-{-
-data Arguments = Arguments
-    { inputFiles :: [String]
-    }
-
-arguments :: Parser Arguments
-arguments = Arguments
-    <$> many (argument str (metavar "TARGET..."))
-
-run_caskell :: Arguments -> IO ()
-run_caskell (Arguments files) = do
-    mapM compile_file files
-    return ()
 
 main :: IO ()
-main = run_caskell =<< execParser opts
-    where
-        opts = info (arguments <**> helper)
-            (fullDesc <> progDesc "Content-Addressed Haskell tool" <> header "caskell - a tool for content-addressed haskell")
-            -}
-main :: IO ()
-main = undefined
+main = run_tests
